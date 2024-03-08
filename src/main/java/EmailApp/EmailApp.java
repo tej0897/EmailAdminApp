6549b2d49dbd78f8f379846012ef56ddd60fd459
tej0897 <tej897@gmail.com>
2024-03-08 19:50:02 +0530

    Final Update

--- src/main/java/EmailApp/EmailApp.java
+++ src/main/java/EmailApp/EmailApp.java
@@ -3,5 +3,7 @@ package EmailApp;
 public class EmailApp {
     public static void main(String[] args) {
         Email emp1 = new Email("John", "Doe");
+        System.out.println(emp1.showInfo());
+
     }
 }
